371ec6d5a8c725ddc0c03eee83975e242b75ab20
SumanthKumar <sumanth.kanuparthi@gmail.com>
2013-12-29 03:26:03 -0500

    Bhavya changes

--- views.py
+++ views.py
@@ -5,8 +5,9 @@ from django_facebook.decorators import facebook_required
 @facebook_required
 def home(request):
   graph = get_persistent_graph(request)
+  me = graph.fql("SELECT friends_about_me,friends_location,friends_hometown FROM permissions WHERE uid = me()")
   #me = graph.fql("SELECT uid, name,current_address,current_location,relationship_status FROM user WHERE uid = me()")
-  me = graph.fql("SELECT uid, name,current_address,current_location,relationship_status FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())")
+  #me = graph.fql("SELECT uid, name,current_address,current_location FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())")
   #me = graph.fql('SELECT name FROM user WHERE uid = me()')
 
   return render(request, 'hellodjango/templates/home.html', {'me':me})
