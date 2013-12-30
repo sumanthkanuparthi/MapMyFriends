2a69a656a55ffa4851a865478c816ad9c279ebe2
SumanthKumar <sumanth.kanuparthi@gmail.com>
2013-12-30 00:35:57 -0500

    version 1.3 - fix for images

--- views.py
+++ views.py
@@ -10,15 +10,9 @@ def home(request):
 
   #me = graph.fql("SELECT friends_about_me,friends_location,friends_hometown FROM permissions WHERE uid = me()")
   me = graph.fql("SELECT uid, name,current_address,current_location,relationship_status FROM user WHERE uid = me()")
-  me1 = graph.fql("SELECT current_location FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())")
+  me1 = graph.fql("SELECT current_location,pic_square FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())")
   #me = graph.fql('SELECT name FROM user WHERE uid = me()')
   #me1 = graph.fql("SELECT uid, name, pic_square FROM user WHERE uid = me() OR uid IN (SELECT uid2 FROM friend WHERE uid1 = me())")
-  pic = graph.fql("SELECT pic_square FROM user WHERE uid = me() OR uid IN (SELECT uid2 FROM friend WHERE uid1 = me())")
-  pics = list()
-  for lis in pic:
-    s = lis.get('pic_square')
-    if s is not None:  
-       pics.append(s.encode('ascii','ignore'))
   
   g = list()
   for li in me1:
@@ -28,9 +22,12 @@ def home(request):
        t.append(s.get('city').encode('ascii','ignore'))
        t.append(s.get('latitude'))
        t.append(s.get('longitude'))
-       g.append(t)  
+       s1 = li.get('pic_square')
+       if s1 is not None:
+         t.append(s1.encode('ascii','ignore'))
+       g.append(t) 
   g = json.dumps(g)
-  return render(request, 'hellodjango/templates/map.html', {'me':me,'me1':me1,'locations':g,'pic':pics})
+  return render(request, 'hellodjango/templates/map.html', {'me':me,'me1':me1,'locations':g})
 
 def pp(request):
   return render(request, 'hellodjango/templates/pp.html')
